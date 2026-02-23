ab6cceba30fa83e0e3e1b5b6b52a2974ce4977dd
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2026-02-23 15:47:00 +0100

    feat: lazy loading y loading screen

--- criptadoge-app/src/renderer/src/App.tsx
+++ criptadoge-app/src/renderer/src/App.tsx
@@ -1,19 +1,30 @@
+import React, { Suspense, lazy } from 'react'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import { Layout } from './components/Layout/Layout'
-import { UsersList } from './components/UsersList/UsersList'
-import { MemberProfile } from './components/MemberProfile/MemberProfile'
-import { Dashboard } from './components/Dashboard/Dashboard'
+import { LoadingScreen } from './components/LoadingScreen/LoadingScreen'
+
+const Layout = lazy(() => import('./components/Layout/Layout').then((m) => ({ default: m.Layout })))
+const Dashboard = lazy(() =>
+  import('./components/Dashboard/Dashboard').then((m) => ({ default: m.Dashboard }))
+)
+const UsersList = lazy(() =>
+  import('./components/UsersList/UsersList').then((m) => ({ default: m.UsersList }))
+)
+const MemberProfile = lazy(() =>
+  import('./components/MemberProfile/MemberProfile').then((m) => ({ default: m.MemberProfile }))
+)
 
 function App() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="socios" element={<UsersList />} />
-          <Route path="socios/:id" element={<MemberProfile />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<LoadingScreen />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="socios" element={<UsersList />} />
+            <Route path="socios/:id" element={<MemberProfile />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </HashRouter>
   )
 }
